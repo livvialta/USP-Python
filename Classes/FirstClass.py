7d3641350191fbc96aff47b4a3d339d1401e0c8d
livvialta <v9lvital@gmail.com>
2023-02-06 16:09:39 -0300

    first commit

---
+++ Classes/FirstClass.py
@@ -0,0 +1,18 @@
+#Receba um valor inteiro, calcule e mostre:
+# - a soma do valor + 1:
+# - a multiplicação d0 valor * 10
+# - a exponenciação do valor ** 10
+
+
+#valor= int(input("Entre com um valor inteiro: \n" ))
+
+#soma=(valor + 1)
+#multiplicacao=(valor * 10)
+#exponenciacao=(valor ** 10)
+
+#print(soma)
+#print(multiplicacao)
+#print(exponenciacao)
+
+#-----------------------------------------------------------
+
